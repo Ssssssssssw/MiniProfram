edcf7a1bcf7cb1f95b4ef61894f8ba5da987b46d
Ssssssssssw <771986333@qq.com>
2020-06-30 00:36:05 +0800

    全局,页面配置

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,28 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:"sun",
+    count: 2,
+    array:[
+      {
+        name: "sun",
+        age: 18,
+        address: "北京市"
+      },
+      {
+        name: "li",
+        age: 28,
+        address: "上海市"
+      },
+      {
+        name: "zhao",
+        age: 38,
+        address: "深圳市"
+      }
+    ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  add(){
+    this.setData({
+        count: this.data.count+1
+    })
   }
 })
